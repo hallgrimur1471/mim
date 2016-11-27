8bd128c558706b0fa207222e1078637c61489eaa
Fannsi <fthg2@hi.is>
2016-11-27 18:39:36 +0000

    hover over marker og thumbnail mynd

--- public/javascripts/leafletFrontend.js
+++ public/javascripts/leafletFrontend.js
@@ -8,18 +8,6 @@ var mymap = L.map('mapid').setView([64.127, -21.817], 13);
     });
     basemap0.addTo(mymap);
     Esri_WorldImagery.addTo(mymap);
-    const position = [64.127, -21.817];
-
-
-    var data = [[64.127, -21.817, 'Thetta er mynd af einhverju'],
-          [64.117, -21.807, 'Thetta er onnur mynd af einhverju'],
-          ];
-
-    for (var i = 0; i < data.length; i++) {
-          marker = new L.marker([data[i][0],data[i][1]])
-          .bindPopup(data[i][2])
-          .addTo(mymap);
-          }
 
     var mynd = L.icon({
           iconUrl: src="geotagged_photo_from_nexus.jpg",
@@ -28,5 +16,30 @@ var mymap = L.map('mapid').setView([64.127, -21.817], 13);
           iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
           popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
           });
+
+    var data = [[64.127, -21.817, 'Thetta er mynd af einhverju'],
+          [64.117, -21.807, 'Thetta er onnur mynd af einhverju'],
+          ];
+
+    for (var i = 0; i < data.length; i++) {
+          marker = new L.marker([data[i][0],data[i][1]])
+          .bindPopup("<img id='image' src='thumbnail.png'/><br>" + data[i][2])
+          .addTo(mymap);
+          marker.on('mouseover', function (e) {
+            this.openPopup();
+          });
+          marker.on('mouseout', function (e) {
+            this.closePopup();
+          });
+          }
+
+        //  marker1.bindPopup( "<img src=" + icon_url + "/> Current temperature in " + location + " is: " + temp_f)
+
+          //"img src=" + icon_url + "geotagged_photo_from_nexus.jpg"
+
+//          marker1.bindPopup( "<img src=" + icon_url + "/> Current temperature in " + location + " is: " + temp_f)
+
+
+
 
           L.marker([64.121, -21.817], {icon: mynd}).addTo(mymap);
