b97e7fccdb3fdb0caeab61f21fae6f83a21f7683
Fannsi <fthg2@hi.is>
2016-11-27 11:36:01 +0000

    loopa i pug

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+//app.listen(3000);
+console.log('Er að keyra upp server');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
