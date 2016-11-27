f63950e366663a64c07c8111318cb834e6517ccc
hallgrimur1471 <hallgrimur1471@gmail.com>
2016-11-27 17:51:36 +0000

    POST images to public/images/

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const multer = require('multer'); // used for uploading files
+const exif = require('./harvestExif.js');
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index_kort', { title: 'Express' });
+});
+
+/* POST image. */
+router.post('/', upload.single('myFile'), function(req, res, next) {
+  console.log(req.body);
+  console.log(req.file);
+  console.log('SERVER FILE NAME\n' + req.file.filename)
+  const destination = req.file.destination;
+  const fileNameOnServer = req.file.filename;
+  exif.getExifData(destination, fileNameOnServer, (result) => {
+    console.log('EXIF DATA:\n' + result);
+    console.log('IMAGE\n' + result.image.Make);
+    res.render('index_kort');
+  }, (error) => {
+    console.log(error);
+    res.render('error', {
+      title: 'Oh no!',
+      message: 'An unexpected error occured when making your request, '
+             + 'perhaps you can try again later.' });
+  });
 });
 
 module.exports = router;
