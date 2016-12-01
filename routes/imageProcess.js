083a5d66dc567aca51415657088585a36b998582
hallgrimur1471 <hallgrimur1471@gmail.com>
2016-12-01 06:48:32 +0000

    Fix

--- routes/imageProcess.js
+++ routes/imageProcess.js
@@ -1,7 +1,6 @@
 const exif = require('./harvestExif.js');
 const imgur = require('imgur-node-api');
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
 
 function processImage(destination, fileNameOnServer, comment, db) {
   const promise = new Promise(
@@ -9,31 +8,26 @@ function processImage(destination, fileNameOnServer, comment, db) {
       exif.getExifData(destination, fileNameOnServer, function exifDataResponse(result) {
         const gps = result.gps;
 
-        const clientIDPath = path.join(__dirname, 'clientID.txt');
-        fs.readFile(clientIDPath, 'utf8', function clientIDResponse(err, data) {
-          if (err) {
-            reject(err);
+        // const clientIDPath = path.join(__dirname, 'clientID.txt');
+        imgur.setClientID(process.env.IMGUR_CLIENT_ID || '06ea0abbf4b0754'); // had to hardcode since heroku will delete clientID.txt on dyno reset
+
+        imgur.upload(destination + fileNameOnServer, function uploadResponse(uerr, ires) {
+          if (uerr) {
+            reject(uerr);
           }
-          imgur.setClientID(process.env.IMGUR_CLIENT_ID || data);
 
-          imgur.upload(destination + fileNameOnServer, function uploadResponse(uerr, ires) {
-            if (uerr) {
-              reject(uerr);
-            }
+          if (!gps.GPSLatitude || !gps.GPSLatitudeRef || !gps.GPSLongitude || !gps.GPSLongitudeRef) {
+            reject('no_exif');
+          }
 
-            if (!gps.GPSLatitude || !gps.GPSLatitudeRef || !gps.GPSLongitude || !gps.GPSLongitudeRef) {
-              reject('no_exif');
-            }
-
-            db.none(`INSERT INTO images (gpslatitude, gpslatituderef, gpslongtitude, gpslongtituderef, comment, imageservername, originalname, deletehash) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-              [gps.GPSLatitude, gps.GPSLatitudeRef, gps.GPSLongitude, gps.GPSLongitudeRef, comment, ires.data.link, fileNameOnServer, ires.data.deletehash])
-          .then((dbdata) => {
-            resolve(dbdata);
-          })
-          .catch((error) => {
-            reject(error);
-          });
-          });
+          db.none(`INSERT INTO images (gpslatitude, gpslatituderef, gpslongtitude, gpslongtituderef, comment, imageservername, originalname, deletehash) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+            [gps.GPSLatitude, gps.GPSLatitudeRef, gps.GPSLongitude, gps.GPSLongitudeRef, comment, ires.data.link, fileNameOnServer, ires.data.deletehash])
+        .then((dbdata) => {
+          resolve(dbdata);
+        })
+        .catch((error) => {
+          reject(error);
+        });
         });
       }, (error) => {
         reject(error);
